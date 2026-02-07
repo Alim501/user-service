b02b9e2d034713aab51219e5d115a5ab494d777f
Alim501 <alimzhan.mamurbekov@mail.ru>
2026-02-07 14:45:18 +0500

    smtp connection family fix

--- back/services/mail.service.js
+++ back/services/mail.service.js
@@ -8,6 +8,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
